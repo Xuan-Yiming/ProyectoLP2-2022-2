f4703f311bd1140bb343b8e01897db1ec27af911
JarumyN <a20181245@pucp.edu.pe>
2022-09-14 09:53:58 -0500

    GestCliente Controller

--- LP2SoftGestClientesController/src/pe/edu/pucp/lp2soft/gestclientes/mysql/ClienteMySQL.java
+++ LP2SoftGestClientesController/src/pe/edu/pucp/lp2soft/gestclientes/mysql/ClienteMySQL.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pe.edu.pucp.lp2soft.gestclientes.mysql;
 
 import java.sql.CallableStatement;
@@ -11,13 +7,9 @@ import java.util.ArrayList;
 import pe.edu.pucp.lp2soft.gestclientes.dao.ClienteDAO;
 import pe.edu.pucp.lp2soft.gestclientes.model.Cliente;
 
-/**
- *
- * @author Jarumy
- */
 public class ClienteMySQL implements ClienteDAO{
     private Connection con;
-    private CallableStatement cs; //LA QUE SE USA PARA LLAMAR A PROCEDIMIENTOS ALMACENADOS, SE USARA PARA LA TA
+    private CallableStatement cs; 
     private ResultSet rs;
 
     @Override
@@ -26,23 +18,11 @@ public class ClienteMySQL implements ClienteDAO{
         try{
             //con = DBManager.getInstance().getConnection();
             
-            cs = con.prepareCall("{call INSERTAR_CLIENTE(?,?,?,?,?,?,?,?,?)}");
-            //cs.registerOutParameter("_id_empleado", java.sql.Types.INTEGER);//REGISTRAMOS EL PARAMETRO DE SALIDA, lo que devuelve
-            //EL PARAMETRO DE SALIDA ES EL UNICO QUE SE REGISTRA DE FORMA DIFERENTE
-            //LOS DEMAS SE REGISTRAN DEPENDIENDO DEL TIPO DE DATO
+            cs = con.prepareCall("{call INSERTAR_CLIENTE(?,?)}");
+            cs.registerOutParameter("_id_cliente", java.sql.Types.INTEGER);
             
-            
-//            cs.setInt("_fid_area", empleado.getArea().getIdArea());
-//            cs.setString("_DNI", empleado.getDNI());
-//            cs.setString("_nombre", empleado.getNombre());
-//            cs.setString("_apellido_paterno", empleado.getApellidoPaterno());
-//            cs.setString("_genero", String.valueOf(empleado.getGenero()));  //SOLO UN CHAR
-//            cs.setDate("_fecha_nacimiento", new java.sql.Date(empleado.getFechaNacimiento().getTime()));    //PARA FECHAS
-//            cs.setString("_cargo", empleado.getCargo());
-//            cs.setDouble("_sueldo", empleado.getSueldo());
-//            resultado = cs.executeUpdate();
-            //****************************************************//
-            //****************************************************//
+            cs.setString("_categoria", cliente.getCategoria());
+            resultado = cs.executeUpdate();
             
         }catch(Exception ex){
             System.out.println(ex.getMessage());
