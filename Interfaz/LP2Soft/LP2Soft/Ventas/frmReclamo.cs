fa94224da3e11c023954e85128d582b0f506cda7
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2022-09-29 05:01:54 -0500

    actualizacion venta

---
+++ Interfaz/LP2Soft/LP2Soft/Ventas/frmReclamo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LP2Soft.Ventas
+{
+    public partial class frmReclamo : Form
+    {
+        public frmReclamo()
+        {
+            InitializeComponent();
+        }
+    }
+}
