8cf44a37f2b916e203276080009376e7df711032
joelAquino18 <aquino.joel@pucp.edu.pe>
2022-09-28 19:30:29 -0500

    Interfaz Usuario

---
+++ Interfaz/LP2Soft/LP2Soft/frmAdministrador.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LP2Soft
+{
+    partial class frmAdministrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btRegresar = new System.Windows.Forms.Button();
+            this.lblFechaIngreso = new System.Windows.Forms.Label();
+            this.dtpFechaIngreso = new System.Windows.Forms.DateTimePicker();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.lblNombreArea = new System.Windows.Forms.Label();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.lblConfirmaciónContraseña = new System.Windows.Forms.Label();
+            this.txtConfirmarPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btRegresar
+            // 
+            this.btRegresar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btRegresar.Location = new System.Drawing.Point(520, 370);
+            this.btRegresar.Name = "btRegresar";
+            this.btRegresar.Size = new System.Drawing.Size(103, 42);
+            this.btRegresar.TabIndex = 54;
+            this.btRegresar.Text = "Regresar";
+            this.btRegresar.UseVisualStyleBackColor = false;
+            // 
+            // lblFechaIngreso
+            // 
+            this.lblFechaIngreso.AutoSize = true;
+            this.lblFechaIngreso.Location = new System.Drawing.Point(225, 276);
+            this.lblFechaIngreso.Name = "lblFechaIngreso";
+            this.lblFechaIngreso.Size = new System.Drawing.Size(115, 16);
+            this.lblFechaIngreso.TabIndex = 53;
+            this.lblFechaIngreso.Text = "Fecha de Ingreso:";
+            // 
+            // dtpFechaIngreso
+            // 
+            this.dtpFechaIngreso.Location = new System.Drawing.Point(346, 271);
+            this.dtpFechaIngreso.Name = "dtpFechaIngreso";
+            this.dtpFechaIngreso.Size = new System.Drawing.Size(255, 22);
+            this.dtpFechaIngreso.TabIndex = 52;
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btAceptar.Location = new System.Drawing.Point(629, 370);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(103, 42);
+            this.btAceptar.TabIndex = 51;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = false;
+            // 
+            // lblNombreArea
+            // 
+            this.lblNombreArea.AutoSize = true;
+            this.lblNombreArea.Location = new System.Drawing.Point(301, 237);
+            this.lblNombreArea.Name = "lblNombreArea";
+            this.lblNombreArea.Size = new System.Drawing.Size(39, 16);
+            this.lblNombreArea.TabIndex = 50;
+            this.lblNombreArea.Text = "Área:";
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(346, 231);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(120, 22);
+            this.txtArea.TabIndex = 49;
+            // 
+            // lblConfirmaciónContraseña
+            // 
+            this.lblConfirmaciónContraseña.AutoSize = true;
+            this.lblConfirmaciónContraseña.Location = new System.Drawing.Point(203, 194);
+            this.lblConfirmaciónContraseña.Name = "lblConfirmaciónContraseña";
+            this.lblConfirmaciónContraseña.Size = new System.Drawing.Size(137, 16);
+            this.lblConfirmaciónContraseña.TabIndex = 48;
+            this.lblConfirmaciónContraseña.Text = "Confirmar contraseña:";
+            // 
+            // txtConfirmarPassword
+            // 
+            this.txtConfirmarPassword.Location = new System.Drawing.Point(346, 191);
+            this.txtConfirmarPassword.Name = "txtConfirmarPassword";
+            this.txtConfirmarPassword.Size = new System.Drawing.Size(148, 22);
+            this.txtConfirmarPassword.TabIndex = 47;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(261, 156);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(79, 16);
+            this.lblPassword.TabIndex = 46;
+            this.lblPassword.Text = "Contraseña:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(346, 153);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(148, 22);
+            this.txtPassword.TabIndex = 45;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(346, 111);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(120, 22);
+            this.txtUsername.TabIndex = 44;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(267, 114);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(73, 16);
+            this.lblUsername.TabIndex = 43;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(244, 37);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(294, 20);
+            this.lblTitulo.TabIndex = 42;
+            this.lblTitulo.Text = "REGISTRO DE ADMINISTRADOR";
+            // 
+            // frmAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btRegresar);
+            this.Controls.Add(this.lblFechaIngreso);
+            this.Controls.Add(this.dtpFechaIngreso);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.lblNombreArea);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.lblConfirmaciónContraseña);
+            this.Controls.Add(this.txtConfirmarPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmAdministrador";
+            this.Text = "frmAdministrador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRegresar;
+        private System.Windows.Forms.Label lblFechaIngreso;
+        private System.Windows.Forms.DateTimePicker dtpFechaIngreso;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Label lblNombreArea;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Label lblConfirmaciónContraseña;
+        private System.Windows.Forms.TextBox txtConfirmarPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ Interfaz/LP2Soft/LP2Soft/frmRegistroUsuario.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace LP2Soft
+{
+    partial class frmRegistroUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegistroUsuario));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ayudaToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lblTipoDoc = new System.Windows.Forms.Label();
+            this.cboTipoDoc = new System.Windows.Forms.ComboBox();
+            this.lblNumDoc = new System.Windows.Forms.Label();
+            this.txtNumDoc = new System.Windows.Forms.TextBox();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.rbSupervisorAlmacen = new System.Windows.Forms.RadioButton();
+            this.rbAdministrador = new System.Windows.Forms.RadioButton();
+            this.rbVendedor = new System.Windows.Forms.RadioButton();
+            this.lblTipoUsuario = new System.Windows.Forms.Label();
+            this.btSiguiente = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(259, 38);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(223, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "REGISTRO DE USUARIO";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.lblID.Location = new System.Drawing.Point(104, 92);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(25, 17);
+            this.lblID.TabIndex = 1;
+            this.lblID.Text = "ID:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayudaToolStripButton,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ayudaToolStripButton
+            // 
+            this.ayudaToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ayudaToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ayudaToolStripButton.Image")));
+            this.ayudaToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ayudaToolStripButton.Name = "ayudaToolStripButton";
+            this.ayudaToolStripButton.Size = new System.Drawing.Size(29, 24);
+            this.ayudaToolStripButton.Text = "Ay&uda";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(51, 24);
+            this.toolStripLabel1.Text = "Ayuda";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(135, 89);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(63, 22);
+            this.txtID.TabIndex = 3;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(70, 135);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(135, 132);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(120, 22);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(135, 174);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(148, 22);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Apellido:";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(69, 221);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(64, 16);
+            this.lblTelefono.TabIndex = 9;
+            this.lblTelefono.Text = "Teléfono:";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(135, 215);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(105, 22);
+            this.txtTelefono.TabIndex = 8;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(85, 258);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
+            this.lblEmail.TabIndex = 11;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(135, 252);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(148, 22);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(62, 296);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(67, 16);
+            this.lblDireccion.TabIndex = 13;
+            this.lblDireccion.Text = "Dirección:";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(135, 290);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(148, 22);
+            this.txtDireccion.TabIndex = 12;
+            // 
+            // lblTipoDoc
+            // 
+            this.lblTipoDoc.AutoSize = true;
+            this.lblTipoDoc.Location = new System.Drawing.Point(364, 92);
+            this.lblTipoDoc.Name = "lblTipoDoc";
+            this.lblTipoDoc.Size = new System.Drawing.Size(129, 16);
+            this.lblTipoDoc.TabIndex = 14;
+            this.lblTipoDoc.Text = "Tipo de Documento:";
+            // 
+            // cboTipoDoc
+            // 
+            this.cboTipoDoc.FormattingEnabled = true;
+            this.cboTipoDoc.Location = new System.Drawing.Point(499, 89);
+            this.cboTipoDoc.Name = "cboTipoDoc";
+            this.cboTipoDoc.Size = new System.Drawing.Size(200, 24);
+            this.cboTipoDoc.TabIndex = 15;
+            // 
+            // lblNumDoc
+            // 
+            this.lblNumDoc.AutoSize = true;
+            this.lblNumDoc.Location = new System.Drawing.Point(344, 132);
+            this.lblNumDoc.Name = "lblNumDoc";
+            this.lblNumDoc.Size = new System.Drawing.Size(149, 16);
+            this.lblNumDoc.TabIndex = 16;
+            this.lblNumDoc.Text = "Número de Documento:";
+            // 
+            // txtNumDoc
+            // 
+            this.txtNumDoc.Location = new System.Drawing.Point(499, 132);
+            this.txtNumDoc.Name = "txtNumDoc";
+            this.txtNumDoc.Size = new System.Drawing.Size(141, 22);
+            this.txtNumDoc.TabIndex = 17;
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(499, 175);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(245, 22);
+            this.dtpFechaNacimiento.TabIndex = 18;
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(355, 180);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(138, 16);
+            this.lblFechaNacimiento.TabIndex = 19;
+            this.lblFechaNacimiento.Text = "Fecha de Nacimiento:";
+            // 
+            // rbSupervisorAlmacen
+            // 
+            this.rbSupervisorAlmacen.AutoSize = true;
+            this.rbSupervisorAlmacen.Location = new System.Drawing.Point(499, 241);
+            this.rbSupervisorAlmacen.Name = "rbSupervisorAlmacen";
+            this.rbSupervisorAlmacen.Size = new System.Drawing.Size(168, 20);
+            this.rbSupervisorAlmacen.TabIndex = 20;
+            this.rbSupervisorAlmacen.TabStop = true;
+            this.rbSupervisorAlmacen.Text = "Supervisor de Almacén";
+            this.rbSupervisorAlmacen.UseVisualStyleBackColor = true;
+            // 
+            // rbAdministrador
+            // 
+            this.rbAdministrador.AutoSize = true;
+            this.rbAdministrador.Location = new System.Drawing.Point(499, 267);
+            this.rbAdministrador.Name = "rbAdministrador";
+            this.rbAdministrador.Size = new System.Drawing.Size(111, 20);
+            this.rbAdministrador.TabIndex = 21;
+            this.rbAdministrador.TabStop = true;
+            this.rbAdministrador.Text = "Administrador";
+            this.rbAdministrador.UseVisualStyleBackColor = true;
+            // 
+            // rbVendedor
+            // 
+            this.rbVendedor.AutoSize = true;
+            this.rbVendedor.Location = new System.Drawing.Point(499, 293);
+            this.rbVendedor.Name = "rbVendedor";
+            this.rbVendedor.Size = new System.Drawing.Size(88, 20);
+            this.rbVendedor.TabIndex = 22;
+            this.rbVendedor.TabStop = true;
+            this.rbVendedor.Text = "Vendedor";
+            this.rbVendedor.UseVisualStyleBackColor = true;
+            // 
+            // lblTipoUsuario
+            // 
+            this.lblTipoUsuario.AutoSize = true;
+            this.lblTipoUsuario.Location = new System.Drawing.Point(386, 221);
+            this.lblTipoUsuario.Name = "lblTipoUsuario";
+            this.lblTipoUsuario.Size = new System.Drawing.Size(107, 16);
+            this.lblTipoUsuario.TabIndex = 23;
+            this.lblTipoUsuario.Text = "Tipo de Usuario:";
+            // 
+            // btSiguiente
+            // 
+            this.btSiguiente.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btSiguiente.Location = new System.Drawing.Point(651, 375);
+            this.btSiguiente.Name = "btSiguiente";
+            this.btSiguiente.Size = new System.Drawing.Size(103, 42);
+            this.btSiguiente.TabIndex = 24;
+            this.btSiguiente.Text = "Siguiente";
+            this.btSiguiente.UseVisualStyleBackColor = false;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btCancelar.Location = new System.Drawing.Point(542, 375);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(103, 42);
+            this.btCancelar.TabIndex = 25;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = false;
+            // 
+            // frmRegistroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btSiguiente);
+            this.Controls.Add(this.lblTipoUsuario);
+            this.Controls.Add(this.rbVendedor);
+            this.Controls.Add(this.rbAdministrador);
+            this.Controls.Add(this.rbSupervisorAlmacen);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.txtNumDoc);
+            this.Controls.Add(this.lblNumDoc);
+            this.Controls.Add(this.cboTipoDoc);
+            this.Controls.Add(this.lblTipoDoc);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmRegistroUsuario";
+            this.Text = "Registro de Usuario";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton ayudaToolStripButton;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label lblTipoDoc;
+        private System.Windows.Forms.ComboBox cboTipoDoc;
+        private System.Windows.Forms.Label lblNumDoc;
+        private System.Windows.Forms.TextBox txtNumDoc;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.RadioButton rbSupervisorAlmacen;
+        private System.Windows.Forms.RadioButton rbAdministrador;
+        private System.Windows.Forms.RadioButton rbVendedor;
+        private System.Windows.Forms.Label lblTipoUsuario;
+        private System.Windows.Forms.Button btSiguiente;
+        private System.Windows.Forms.Button btCancelar;
+    }
+}
