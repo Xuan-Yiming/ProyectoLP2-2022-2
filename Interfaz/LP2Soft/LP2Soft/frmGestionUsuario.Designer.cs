382ee582725410f29099853975624bbbfa755757
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2022-09-29 00:38:29 -0500

    clean build

--- Interfaz/LP2Soft/LP2Soft/frmGestionUsuario.Designer.cs
+++ Interfaz/LP2Soft/LP2Soft/frmGestionUsuario.Designer.cs
@@ -40,9 +40,10 @@
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ForeColor = System.Drawing.Color.Black;
-            this.lblTitulo.Location = new System.Drawing.Point(275, 34);
+            this.lblTitulo.Location = new System.Drawing.Point(206, 28);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(222, 20);
+            this.lblTitulo.Size = new System.Drawing.Size(187, 17);
             this.lblTitulo.TabIndex = 27;
             this.lblTitulo.Text = "GESTIÓN DE USUARIOS";
             // 
@@ -52,17 +53,19 @@
             this.panel1.Controls.Add(this.lblTitulo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 89);
+            this.panel1.Size = new System.Drawing.Size(600, 72);
             this.panel1.TabIndex = 28;
             // 
             // btnRegistrar
             // 
             this.btnRegistrar.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrar.Location = new System.Drawing.Point(162, 153);
+            this.btnRegistrar.Location = new System.Drawing.Point(122, 124);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(199, 103);
+            this.btnRegistrar.Size = new System.Drawing.Size(149, 84);
             this.btnRegistrar.TabIndex = 29;
             this.btnRegistrar.Text = "Registrar Usuario";
             this.btnRegistrar.UseVisualStyleBackColor = false;
@@ -71,21 +74,23 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(456, 153);
+            this.button2.Location = new System.Drawing.Point(342, 124);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(199, 103);
+            this.button2.Size = new System.Drawing.Size(149, 84);
             this.button2.TabIndex = 30;
             this.button2.Text = "Modificar Usuario";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // frmGestionUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmGestionUsuario";
             this.Text = "Gestion de Usuario";
             this.panel1.ResumeLayout(false);
