19c907c0eef72ec4a883c11457f612f6d780bb08
joelAquino18 <aquino.joel@pucp.edu.pe>
2022-09-28 20:42:44 -0500

    actualizacion interfaz usuario

---
+++ Interfaz/LP2Soft/LP2Soft/frmBusquedaUsuario.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace LP2Soft
+{
+    partial class frmBusquedaUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombreID = new System.Windows.Forms.Label();
+            this.txtNombreID = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCompleto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombreID
+            // 
+            this.lblNombreID.AutoSize = true;
+            this.lblNombreID.Location = new System.Drawing.Point(11, 23);
+            this.lblNombreID.Name = "lblNombreID";
+            this.lblNombreID.Size = new System.Drawing.Size(214, 16);
+            this.lblNombreID.TabIndex = 0;
+            this.lblNombreID.Text = "Ingrese el nombre o ID del usuario:";
+            // 
+            // txtNombreID
+            // 
+            this.txtNombreID.Location = new System.Drawing.Point(231, 20);
+            this.txtNombreID.Name = "txtNombreID";
+            this.txtNombreID.Size = new System.Drawing.Size(321, 22);
+            this.txtNombreID.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(579, 18);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(101, 26);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Location = new System.Drawing.Point(686, 18);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(101, 26);
+            this.btnSeleccionar.TabIndex = 3;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.AllowUserToAddRows = false;
+            this.dgvUsuarios.AllowUserToDeleteRows = false;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.NombreCompleto,
+            this.TipoUsuario});
+            this.dgvUsuarios.Location = new System.Drawing.Point(15, 64);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.ReadOnly = true;
+            this.dgvUsuarios.RowHeadersWidth = 51;
+            this.dgvUsuarios.RowTemplate.Height = 24;
+            this.dgvUsuarios.Size = new System.Drawing.Size(772, 374);
+            this.dgvUsuarios.TabIndex = 4;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 70;
+            // 
+            // NombreCompleto
+            // 
+            this.NombreCompleto.HeaderText = "Nombre Completo";
+            this.NombreCompleto.MinimumWidth = 6;
+            this.NombreCompleto.Name = "NombreCompleto";
+            this.NombreCompleto.ReadOnly = true;
+            this.NombreCompleto.Width = 200;
+            // 
+            // TipoUsuario
+            // 
+            this.TipoUsuario.HeaderText = "Tipo de Usuario";
+            this.TipoUsuario.MinimumWidth = 6;
+            this.TipoUsuario.Name = "TipoUsuario";
+            this.TipoUsuario.ReadOnly = true;
+            this.TipoUsuario.Width = 150;
+            // 
+            // frmBusquedaUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNombreID);
+            this.Controls.Add(this.lblNombreID);
+            this.Name = "frmBusquedaUsuario";
+            this.Text = "Búsqueda de Usuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombreID;
+        private System.Windows.Forms.TextBox txtNombreID;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCompleto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoUsuario;
+    }
+}
