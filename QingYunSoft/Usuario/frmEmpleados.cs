b51cfe749270b879112c7fdd7735c89ee5a8f418
joelAquino18 <aquino.joel@pucp.edu.pe>
2022-10-26 13:11:59 -0500

    Avance CSharp

--- QingYunSoft/Usuario/frmEmpleados.cs
+++ QingYunSoft/Usuario/frmEmpleados.cs
@@ -10,8 +10,11 @@ using System.Windows.Forms;
 
 namespace QingYunSoft.Usuario
 {
+
     public partial class frmEmpleados : Form
     {
+        private RRHHWS.RRHHWSClient daoRRHH;
+
         public frmEmpleados()
         {
             InitializeComponent();
